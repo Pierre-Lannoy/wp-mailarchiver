be3fbf0139778f3e0730032dc67d18991bf75dd2
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-01-22 22:55:14 +0100

    Fixes namespaces.

--- includes/system/class-cache.php
+++ includes/system/class-cache.php
@@ -111,8 +111,8 @@ class Cache {
 			wp_cache_add_global_groups( self::$pool_name );
 		}
 		self::$apcu_available = function_exists( 'apcu_delete' ) && function_exists( 'apcu_fetch' ) && function_exists( 'apcu_store' );
-		add_action( 'shutdown', [ 'Traffic\System\Cache', 'log_debug' ], 10, 0 );
-		add_filter( 'perfopsone_icache_introspection', [ 'Traffic\System\Cache', 'introspection' ] );
+		add_action( 'shutdown', [ 'MailArchiver\System\Cache', 'log_debug' ], 10, 0 );
+		add_filter( 'perfopsone_icache_introspection', [ 'MailArchiver\System\Cache', 'introspection' ] );
 	}
 
 	/**
@@ -121,7 +121,7 @@ class Cache {
 	 * @since 1.0.0
 	 */
 	public static function introspection( $endpoints ) {
-		$endpoints[ TRAFFIC_SLUG ] = [ 'name' => TRAFFIC_PRODUCT_NAME, 'version' => TRAFFIC_VERSION, 'endpoint' => [ 'Traffic\System\Cache', 'get_analytics' ] ];
+		$endpoints[ MAILARCHIVER_SLUG ] = [ 'name' => MAILARCHIVER_PRODUCT_NAME, 'version' => MAILARCHIVER_VERSION, 'endpoint' => [ 'MailArchiver\System\Cache', 'get_analytics' ] ];
 		return $endpoints;
 	}
 
