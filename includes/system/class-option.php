4c71a215cdd8127ffeb89e4966f201eddf72c102
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-04-01 01:12:38 +0200

    Override plugins defaults values for getters if $default is set in call.

--- includes/system/class-option.php
+++ includes/system/class-option.php
@@ -119,8 +119,6 @@ class Option {
 	 *
 	 * @param   string  $option     Option name. Expected to not be SQL-escaped.
 	 * @param   boolean $default    Optional. The default value if option doesn't exists.
-	 *                              This default value is used only if $option is not present
-	 *                              in the $defaults array.
 	 * @return  mixed   The value of the option.
 	 * @since 1.0.0
 	 */
@@ -136,8 +134,6 @@ class Option {
 	 *
 	 * @param   string  $option     Option name. Expected to not be SQL-escaped.
 	 * @param   boolean $default    Optional. The default value if option doesn't exists.
-	 *                              This default value is used only if $option is not present
-	 *                              in the $defaults array.
 	 * @return  mixed   The value of the option.
 	 * @since 1.0.0
 	 */
