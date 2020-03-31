2ffac0bc5bea4fc7d8255e67be8534128976b603
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-04-01 01:05:13 +0200

    Override plugins defaults values for getters if $default is set in call.

--- includes/system/class-option.php
+++ includes/system/class-option.php
@@ -125,7 +125,7 @@ class Option {
 	 * @since 1.0.0
 	 */
 	public static function site_get( $option, $default = null ) {
-		if ( array_key_exists( $option, self::$defaults ) ) {
+		if ( array_key_exists( $option, self::$defaults ) && ! isset( $default ) ) {
 			$default = self::$defaults[ $option ];
 		}
 		return get_option( MAILARCHIVER_PRODUCT_ABBREVIATION . '_' . $option, $default );
@@ -142,7 +142,7 @@ class Option {
 	 * @since 1.0.0
 	 */
 	public static function network_get( $option, $default = null ) {
-		if ( array_key_exists( $option, self::$defaults ) ) {
+		if ( array_key_exists( $option, self::$defaults ) && ! isset( $default ) ) {
 			$default = self::$defaults[ $option ];
 		}
 		return get_site_option( MAILARCHIVER_PRODUCT_ABBREVIATION . '_' . $option, $default );
