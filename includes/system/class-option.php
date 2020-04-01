19e7754b4b629aec103cc5cc03f787cc403d5c56
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-04-01 17:13:17 +0200

    Fixes issue about boolean default value.

--- includes/system/class-option.php
+++ includes/system/class-option.php
@@ -126,7 +126,11 @@ class Option {
 		if ( array_key_exists( $option, self::$defaults ) && ! isset( $default ) ) {
 			$default = self::$defaults[ $option ];
 		}
-		return get_option( MAILARCHIVER_PRODUCT_ABBREVIATION . '_' . $option, $default );
+		$val = get_option( MAILARCHIVER_PRODUCT_ABBREVIATION . '_' . $option, $default );
+		if ( empty( $val ) && is_bool( $default ) ) {
+			return $default;
+		}
+		return $val;
 	}
 
 	/**
@@ -141,7 +145,11 @@ class Option {
 		if ( array_key_exists( $option, self::$defaults ) && ! isset( $default ) ) {
 			$default = self::$defaults[ $option ];
 		}
-		return get_site_option( MAILARCHIVER_PRODUCT_ABBREVIATION . '_' . $option, $default );
+		$val = get_site_option( MAILARCHIVER_PRODUCT_ABBREVIATION . '_' . $option, $default );
+		if ( empty( $val ) && is_bool( $default ) ) {
+			return $default;
+		}
+		return $val;
 	}
 
 	/**
