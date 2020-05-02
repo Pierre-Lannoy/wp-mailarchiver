1c9a7be36e545b4524f04f94e5d8a506caf7765e
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-05-02 12:54:43 +0200

    Fixes issues with IIS/Win10.

--- includes/system/class-environment.php
+++ includes/system/class-environment.php
@@ -35,6 +35,7 @@ class Environment {
 	 */
 	public static function init() {
 		$plugin_path         = str_replace( MAILARCHIVER_SLUG . '/includes/system/', MAILARCHIVER_SLUG . '/', plugin_dir_path( __FILE__ ) );
+		$plugin_path         = str_replace( MAILARCHIVER_SLUG . '\includes\system/', MAILARCHIVER_SLUG . '/', $plugin_path );
 		$plugin_url          = str_replace( MAILARCHIVER_SLUG . '/includes/system/', MAILARCHIVER_SLUG . '/', plugin_dir_url( __FILE__ ) );
 		$plugin_relative_url = str_replace( get_site_url() . '/', '', $plugin_url );
 		define( 'MAILARCHIVER_PLUGIN_DIR', $plugin_path );
