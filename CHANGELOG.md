6ed90ee3fe98357bf10394c4f0f5faba114abcea
Pierre Lannoy <pierre@lannoy.fr>
2021-12-02 14:04:53 +0100

    Ready to release 2.6.0 on wp.org

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,7 @@ All notable changes to **MailArchiver** are documented in this *changelog*.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and **MailArchiver** adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [2.6.0] - Not yet released
+## [2.6.0] - 2021-12-07
 
 ### Added
 - Compatibility with WordPress 5.9.
--- init.php
+++ init.php
@@ -12,7 +12,7 @@ define( 'MAILARCHIVER_PRODUCT_URL', 'https://github.com/Pierre-Lannoy/wp-mailarc
 define( 'MAILARCHIVER_PRODUCT_SHORTNAME', 'MailArchiver' );
 define( 'MAILARCHIVER_PRODUCT_ABBREVIATION', 'mailarchiver' );
 define( 'MAILARCHIVER_SLUG', 'mailarchiver' );
-define( 'MAILARCHIVER_VERSION', '2.6.0-dev0' );
+define( 'MAILARCHIVER_VERSION', '2.6.0' );
 define( 'MAILARCHIVER_MONOLOG_VERSION', '2.3.4' );
 define( 'MAILARCHIVER_CODENAME', '"-"' );
 define( 'MAILARCHIVER_CRON_NAME', 'mailarchiver_clean_database' );
